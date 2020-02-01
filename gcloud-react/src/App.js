a6faec4249be4755d89e326b42e03303d8c34d2b
Sean Kerr <takuyakerr@gmail.com>
2020-02-01 17:12:40 -0500

    MORE REFORMATTING

--- gcloud-react/src/App.js
+++ gcloud-react/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
 
 function App() {
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Please Help Ryan Kerr is at my Door.
+          Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Fool, you fell for the classic blunder
+          Learn React
         </a>
       </header>
     </div>
